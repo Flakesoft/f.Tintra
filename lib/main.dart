bf5758ccd4cafd0a645ec703d73430a6feb06ed6
Zoran <batajeremiccc@gmail.com>
2026-07-10 20:43:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,23 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FTintraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FTintraApp extends StatelessWidget {
+  const FTintraApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'f.Tintra',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorSchemeSeed: Colors.blue,
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('f.Tintra'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'f.Tintra',
-              style: TextStyle(
-                fontSize: 64,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(height: 24),
-            Text(
-              'Coming soon',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.grey,
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
