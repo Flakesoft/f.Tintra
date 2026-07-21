3a556dd9fccc80dde9ca03df2f4b7dc1291fd1a4
Zoran <batajeremiccc@gmail.com>
2026-07-21 18:27:24 +0200

    Update image_state.dart

--- lib/models/image_state.dart
+++ lib/models/image_state.dart
@@ -1,25 +1,31 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 
 class ImageState {
   final String path;
   final img.Image image;
+  final Uint8List previewBytes;
   final Color? selectedColor;
 
   const ImageState({
     required this.path,
     required this.image,
+    required this.previewBytes,
     this.selectedColor,
   });
 
   ImageState copyWith({
     String? path,
     img.Image? image,
+    Uint8List? previewBytes,
     Color? selectedColor,
   }) {
     return ImageState(
       path: path ?? this.path,
       image: image ?? this.image,
+      previewBytes: previewBytes ?? this.previewBytes,
       selectedColor: selectedColor ?? this.selectedColor,
     );
   }
